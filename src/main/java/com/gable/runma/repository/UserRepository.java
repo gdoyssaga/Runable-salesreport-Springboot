fc6e26f7499ae14b8a297d9210c8fc6c232fbcfc
Parit T <parit.trin@gmail.com>
2023-03-08 00:00:14 +0700

    made change on user controller and repo due to rebase

--- src/main/java/com/gable/runma/repository/UserRepository.java
+++ src/main/java/com/gable/runma/repository/UserRepository.java
@@ -6,5 +6,6 @@ import com.gable.runma.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    public User findByEmail(String email);
+	User findByEmail(String email);
+
 }
