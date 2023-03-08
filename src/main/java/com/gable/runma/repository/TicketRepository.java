519d268448e22df8aa9a7e99381cdbe1fcc30d95
ploy <ploy@gmail.com>
2023-03-08 10:45:54 +0700

    Add findByStatusAndRaceType()

--- src/main/java/com/gable/runma/repository/TicketRepository.java
+++ src/main/java/com/gable/runma/repository/TicketRepository.java
@@ -1,9 +1,15 @@
 package com.gable.runma.repository;
 
+import com.gable.runma.model.RaceType;
+import com.gable.runma.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gable.runma.model.Ticket;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+    List<Ticket> findByStatusAndRaceType(Status status, RaceType r );
 
 }
