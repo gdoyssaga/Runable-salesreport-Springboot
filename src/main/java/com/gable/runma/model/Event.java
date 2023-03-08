6dfda6376027cfaefbc3f84ad8631efe7627fc64
ploy <ploy@gmail.com>
2023-03-08 10:55:42 +0700

    Add  Integer Capacity;

--- src/main/java/com/gable/runma/model/Event.java
+++ src/main/java/com/gable/runma/model/Event.java
@@ -1,52 +1,45 @@
 package com.gable.runma.model;
-import java.util.*;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinTable;
-import javax.persistence.JoinColumn;
-
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-
 import lombok.Data;
 
-/**
- * 
- */
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
 @Data
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler" })
 public class Event {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
-    private String location;
-    private String tag;
     @Temporal(TemporalType.DATE)
     private Date raceDate;
     @Temporal(TemporalType.DATE)
     private Date openRegisDate;
     @Temporal(TemporalType.DATE)
     private Date closeRegisDate;
-    @OneToMany (mappedBy = "event" ,fetch = FetchType.LAZY ,cascade = CascadeType.REMOVE )    
+    private Boolean outOfTicketFlag;
+    private String province;
+    private String location;
+    private Integer capacity;
+
+    @OneToMany (
+            mappedBy = "event" ,fetch = FetchType.LAZY,
+            cascade = {CascadeType.REMOVE,CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}
+            )
     private List<RaceType> raceTypeList;
-    @ManyToMany
-    @JoinTable(name = "EVENT_ORGANIZER",  joinColumns =  @JoinColumn(name = "event_id") ,
-            inverseJoinColumns =  @JoinColumn(name = "organizer_id") )
+
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = {CascadeType.MERGE, CascadeType.REFRESH}
+            )
+    @JoinTable(name = "EventOrganizer",  joinColumns =  @JoinColumn(name = "EventId", referencedColumnName = "id") ,
+            inverseJoinColumns =  @JoinColumn(name = "OrganizerId", referencedColumnName = "id") )
     private List<Organizer> organizerList;
 
 }
